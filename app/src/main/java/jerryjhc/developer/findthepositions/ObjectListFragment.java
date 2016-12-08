66ef2da6a90cbff02990100806114c0e2d2002ce
JerryJHC <jerryhenriquez@gmail.com>
2016-12-08 12:28:54 +0100

    Add fragments, header and strings.

---
+++ app/src/main/java/jerryjhc/developer/findthepositions/ObjectListFragment.java
@@ -0,0 +1,18 @@
+package jerryjhc.developer.findthepositions;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ObjectListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.object_list, container, false);
+        return view;
+    }
+}
